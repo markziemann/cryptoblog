9f846d1cb5faa1188b23630d08f4e6a4f42d174b
markziemann <mark.ziemann@gmail.com>
2021-07-22 00:51:30 +1000

    push notifications are better than email

--- daily.sh
+++ daily.sh
@@ -7,13 +7,6 @@ cd $MYDIR
 Rscript -e "rmarkdown::render('alerts.Rmd')"
 cp alerts.html crypto
 
-MASIGNAL=$(grep SIGNAL_ alerts.html | grep '<pre><code>' \
-| grep -v 'SIGNAL_NONE' | wc -l)
-
-if [ "$MASIGNAL" -gt 0 ] ; then
-
-  echo "There is a new crypto market alert - visit https://blinder.cc/crypto/alerts.html for more details" \
-  | mail -s "Crypto market alert" mark.ziemann@gmail.com
-
-fi
+Rscript -e "rmarkdown::render('coins.Rmd')"
+cp coins.html crypto
 
