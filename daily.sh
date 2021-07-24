28a57042bb8de3c7d2666fd661c502246893884f
markziemann <mark.ziemann@gmail.com>
2021-07-25 00:19:09 +1000

    earlier alerts

--- daily.sh
+++ daily.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+#55 9 * * * bash /home/pi/projects/cryptoblog/daily.sh
 MYCMD=$(echo $0)
 MYDIR=$(dirname $MYCMD)
 cd $MYDIR
 
 Rscript -e "rmarkdown::render('alerts.Rmd')"
 cp alerts.html crypto
-
+sleep 11m
 Rscript -e "rmarkdown::render('coins.Rmd')"
 cp coins.html crypto
 
