926a5351f3ca6d000e61b3ca1eea9c21f439955f
markziemann <mark.ziemann@gmail.com>
2021-11-05 21:53:03 +1100

    eth

--- daily.sh
+++ daily.sh
@@ -9,3 +9,11 @@ cp alerts.html crypto
 Rscript -e "rmarkdown::render('coins.Rmd')"
 cp coins.html crypto
 
+Rscript -e "rmarkdown::render('ma_trading.Rmd')"
+scp -P 21098 ma_trading.html mdzascfs@mdz-analytics.com:/home/mdzascfs/public_html/crypto
+
+Rscript -e "rmarkdown::render('ma_trading_eth.Rmd')"
+scp -P 21098 ma_trading_eth.html mdzascfs@mdz-analytics.com:/home/mdzascfs/public_html/crypto
+
+
+
