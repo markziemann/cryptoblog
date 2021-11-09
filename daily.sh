2e7499eddc32e21fd383086ea7280cf48d0c172a
markziemann <mark.ziemann@gmail.com>
2021-11-09 23:51:03 +1100

    alerts

--- daily.sh
+++ daily.sh
@@ -4,16 +4,24 @@ MYCMD=$(echo $0)
 MYDIR=$(dirname $MYCMD)
 cd $MYDIR
 
-Rscript -e "rmarkdown::render('alerts.Rmd')"
+Rscript -e "rmarkdown::render('alerts.Rmd')" && \
+scp -P 21098 alerts.html mdzascfs@mdz-analytics.com:/home/mdzascfs/public_html/crypto
 cp alerts.html crypto
+
 Rscript -e "rmarkdown::render('coins.Rmd')"
 cp coins.html crypto
 
-Rscript -e "rmarkdown::render('ma_trading.Rmd')"
-scp -P 21098 ma_trading.html mdzascfs@mdz-analytics.com:/home/mdzascfs/public_html/crypto
+Rscript -e "rmarkdown::render('ma_trading.Rmd')" && \
+  scp -P 21098 ma_trading.html mdzascfs@mdz-analytics.com:/home/mdzascfs/public_html/crypto
 
-Rscript -e "rmarkdown::render('ma_trading_eth.Rmd')"
-scp -P 21098 ma_trading_eth.html mdzascfs@mdz-analytics.com:/home/mdzascfs/public_html/crypto
+Rscript -e "rmarkdown::render('ma_trading_eth.Rmd')" && \
+  scp -P 21098 ma_trading_eth.html mdzascfs@mdz-analytics.com:/home/mdzascfs/public_html/crypto
 
+Rscript -e "rmarkdown::render('ma_trading_weekly.Rmd')" && \
+  scp -P 21098 ma_trading_weekly.html mdzascfs@mdz-analytics.com:/home/mdzascfs/public_html/crypto
 
+Rscript -e "rmarkdown::render('ma_trading_weekly_eth.Rmd')" && \
+  scp -P 21098 ma_trading_weekly_eth.html mdzascfs@mdz-analytics.com:/home/mdzascfs/public_html/crypto
 
+Rscript -e "rmarkdown::render('stablecoincap.Rmd')" && \
+  scp -P 21098 stablecoincap.html mdzascfs@mdz-analytics.com:/home/mdzascfs/public_html/crypto
