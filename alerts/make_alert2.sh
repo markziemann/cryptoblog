bca1b8090a906c66459bb8823cd2a6b29c3522ea
markziemann <mark.ziemann@gmail.com>
2022-09-30 12:14:00 +0000

    id

---
+++ alerts/make_alert2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# first get a list of coins that have backtesting data.
+#find ../coin_script/ \
+#| grep summary_ | grep html$ \
+#| rev | cut -d '/' -f1 | rev \
+#| cut -d '.' -f1 | cut -d '_' -f2 > coins.txt
+
+# now make the alerts_xx.Rmd script for each
+for COIN in `cat coins2.txt` ; do
+
+  echo $COIN
+  coin=$(echo $COIN | tr '[:upper:]' '[:lower:]' )
+  sed "s/TRX/${COIN}/g" alerts_TRX.Rmd \
+  | sed "s/trx/${coin}/g" > tmp
+  mv tmp alerts_${COIN}.Rmd
+
+done
